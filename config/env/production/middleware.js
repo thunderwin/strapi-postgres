6f9eaca69405eaa1187b5939c1f10a9c30dc869b
Nathan Lei <woooms@qq.com>
2021-09-17 16:01:12 +0800

    fixfix upload

--- config/env/production/middleware.js
+++ config/env/production/middleware.js
@@ -3,5 +3,12 @@ module.exports = {
     logger: {
       level: "error",
     },
+    parser: {
+      enabled: true,
+      multipart: true,
+      formidable: {
+        maxFileSize: 200 * 1024 * 1024, // Defaults to 200mb
+      },
+    },
   },
 };

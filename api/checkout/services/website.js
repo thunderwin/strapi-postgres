025cbcac2f4fc1fbe0851efd05640772526a3637
Nathan Lei <woooms@qq.com>
2021-10-12 08:17:51 +0800

    asas

--- api/checkout/services/website.js
+++ api/checkout/services/website.js
@@ -8,6 +8,15 @@ const websites = {
         "EFZrIisWYYC8jUYZSPWjJ6H5OItKYPuEDdUyn_avva8Hh4eLG34Kre2MxL7ZgPOtxvEqOOcrN863q_hi",
     },
   },
+  "ivchicy.myshopify.com": {
+    paypalConfig: {
+      paypalAccount: "free.lei.sheng-facilitator@gmail.com",
+      paypalId:
+        "ASnwz_T0VEvmifWp9MkaKro84lGBjdhNcUAw7lrWeVcNAeg2PzdqY32QxN9wFoxVLwTYmGNKgEJbpilk",
+      paypalKey:
+        "EFZrIisWYYC8jUYZSPWjJ6H5OItKYPuEDdUyn_avva8Hh4eLG34Kre2MxL7ZgPOtxvEqOOcrN863q_hi",
+    },
+  },
 };
 
 module.exports = function (domain) {

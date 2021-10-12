a73ab17c18b6f5021a8d9769ae46414ad8aa1974
Nathan Lei <woooms@qq.com>
2021-10-12 15:56:53 +0800

    asas

--- api/checkout/services/website.js
+++ api/checkout/services/website.js
@@ -17,8 +17,27 @@ const websites = {
         "EFZrIisWYYC8jUYZSPWjJ6H5OItKYPuEDdUyn_avva8Hh4eLG34Kre2MxL7ZgPOtxvEqOOcrN863q_hi",
     },
   },
+  "www.ivchicy.com": {
+    paypalConfig: {
+      paypalAccount: "free.lei.sheng-facilitator@gmail.com",
+      paypalId:
+        "ASnwz_T0VEvmifWp9MkaKro84lGBjdhNcUAw7lrWeVcNAeg2PzdqY32QxN9wFoxVLwTYmGNKgEJbpilk",
+      paypalKey:
+        "EFZrIisWYYC8jUYZSPWjJ6H5OItKYPuEDdUyn_avva8Hh4eLG34Kre2MxL7ZgPOtxvEqOOcrN863q_hi",
+    },
+  },
 };
 
 module.exports = function (domain) {
-  return websites[domain];
+  return (
+    websites[domain] || {
+      paypalConfig: {
+        paypalAccount: "free.lei.sheng-facilitator@gmail.com",
+        paypalId:
+          "ASnwz_T0VEvmifWp9MkaKro84lGBjdhNcUAw7lrWeVcNAeg2PzdqY32QxN9wFoxVLwTYmGNKgEJbpilk",
+        paypalKey:
+          "EFZrIisWYYC8jUYZSPWjJ6H5OItKYPuEDdUyn_avva8Hh4eLG34Kre2MxL7ZgPOtxvEqOOcrN863q_hi",
+      },
+    }
+  );
 };

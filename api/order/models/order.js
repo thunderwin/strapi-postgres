f31436198ddd0bba2a70b4880cbbcbed47f2830e
Nathan Lei <woooms@qq.com>
2021-10-19 18:13:50 +0800

    sds

--- api/order/models/order.js
+++ api/order/models/order.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/models.html#lifecycle-hooks)
@@ -10,23 +10,26 @@ module.exports = {
     // Called before an entry is created
     beforeCreate(data) {},
     // Called after an entry is created
-    async afterUpdate(order, params, data) {
+    afterUpdate(order, params, data) {
       // console.dir('创建新订单结果')
       // console.log(JSON.stringify(order))
 
-      if (order.active ===  false){
-        await strapi.plugins['email'].services.email.send({
-          to: 'woooms@qq.com',
-          from: 'nathan@qq.com',
-          subject: 'test email',
-          text: `
-            The comment #${order.id} contain a bad words.
-            Comment:
-            ${order.content}
-          `,
-        });
+      if (order.active === false) {
+        try {
+          strapi.plugins["email"].services.email.send({
+            to: "woooms@qq.com",
+            from: "nathan@qq.com",
+            subject: "test email",
+            text: `
+              The comment #${order.id} contain a bad words.
+              Comment:
+              ${order.content}
+            `,
+          });
+        } catch (error) {
+          return "email fault";
+        }
       }
-
     },
   },
 };

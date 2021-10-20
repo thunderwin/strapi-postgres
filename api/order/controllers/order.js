3281aada67a1bc8d5d3e3a3eb3448659084d0331
Nathan Lei <woooms@qq.com>
2021-10-20 17:21:32 +0800

    修改显示

--- api/order/controllers/order.js
+++ api/order/controllers/order.js
@@ -1,8 +1,59 @@
-'use strict';
-
+"use strict";
+const Joi = require("joi");
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  sync: async (ctx) => {
+    let body = ctx.query;
+
+    const schema = Joi.object({
+      from: Joi.date().required(),
+      to: Joi.date().required(),
+      start: Joi.number().min(0).required(),
+    });
+
+    const { error, value } = schema.validate(body);
+
+    if (error) {
+      return ctx.send(error.details);
+    }
+
+    let r = await strapi.query("order").find({
+      created_at_lt: value.to,
+      created_at_gt: value.from,
+      active: false,
+
+      _limit: 3,
+      _sort: "id",
+      _start: value.start,
+    });
+
+    return ctx.send(r);
+  },
+
+  dayCount: async (ctx) => {
+    let body = ctx.query;
+
+    const schema = Joi.object({
+      from: Joi.date().required(),
+      to: Joi.date().required(),
+    });
+
+    const { error, value } = schema.validate(body);
+
+    if (error) {
+      return ctx.send(error.details);
+    }
+
+    let r = await strapi.query("order").find({
+      created_at_lt: value.to,
+      created_at_gt: value.from,
+      active: false,
+    });
+
+    return ctx.send(r);
+  },
+};

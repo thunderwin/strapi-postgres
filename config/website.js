f8c391c8e51e598704cb1045d3eb2db4efe14e3d
Nathan Lei <woooms@qq.com>
2021-11-18 18:07:24 +0800

    dfd

--- config/website.js
+++ config/website.js
@@ -44,6 +44,21 @@ const websites = {
       capiTestId: "TEST90933",
     },
   },
+  "novachique.com": {
+    paypalConfig: {
+      paypalAccount: "waterCaltrophappy@163.com",
+      paypalId:
+        "AaGQbqTi9H8CY2R4cb4w0rHMQJWl0sqUNAo2p8YhN3aCtYP--ioBlsuF7mOmTeJsC9t3pTSiRQWq8mTu",
+      paypalKey:
+        "EIl7havEk0wkW4DbcATFkAFtijm1ZktOLoZ09N_l5Ljvm3EkbN-VjJWr6UH01Y_Up9Lthppqyy2oQyI3",
+    },
+    facebookConfig: {
+      pixelId: "110986787786144",
+      pixelAccessToken:
+        "EAAny9poEXpcBAK2oKjlpINt2TbA6cDVTPvEaMyLI6yxObxwONc8I2XkMblfJTDcm8pJLrr9XATxHu7KQVaBSuEvZA8zZCvgxcsE9OOtiCUwYNmUBtDpTNVAy3piugOMqC2fc8eVoQAZC3d4SafM96xXGvBv4MsqjZBQAcd6sIBePt9F0PORMiUgIcqwoKIAZD",
+      capiTestId: "TEST90933",
+    },
+  },
   "wudizu.myshopify.com": {
     paypalConfig: {
       paypalAccount: "free.lei.sheng-facilitator@gmail.com",

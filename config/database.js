0ee55fba0a3d620ed41da5a1ced5581687772b6f
Nathan Lei <woooms@qq.com>
2021-11-22 21:27:27 +0800

    asa

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const { parse } = require("pg-connection-string");
 
 module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(env("DATABASE_URL_DEV"));
+  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
 
   return {
     defaultConnection: "default",

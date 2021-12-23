aa88bb358ab23a08334447384d392d47b1158035
Nathan Lei <woooms@qq.com>
2021-12-23 19:07:39 +0800

    asa

--- api/email-template/services/email-template.js
+++ api/email-template/services/email-template.js
@@ -57,9 +57,7 @@ function genEmailTemplate(template, order) {
 module.exports = {
   async sendEmail({ orderId, templateId }) {
 
-    console.dir('收到发送邮件请求')
-    console.log(JSON.stringify(orderId))
-    console.log(JSON.stringify(templateId))
+
     let r = await Promise.all([
       strapi.query("order").findOne({ id: orderId }),
       strapi.query("email-template").findOne({ id: templateId }),
@@ -85,8 +83,7 @@ module.exports = {
 
     try {
      let r = await sgMail.send(msg);
-     console.dir('发送邮件成功')
-     console.log(JSON.stringify(r))
+
 
       return r;
     } catch (e){

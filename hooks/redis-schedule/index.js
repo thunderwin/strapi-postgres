61647c2974eaa5e701864be5e44c224b74d3cb3c
josley <593937170@qq.com>
2021-12-23 17:02:59 +0800

    feat: complete schedule task tests

--- hooks/redis-schedule/index.js
+++ hooks/redis-schedule/index.js
@@ -14,14 +14,14 @@ module.exports = (strapi) => {
     async initialize() {
       console.log("hooks loaded");
       strapi.services["redis-schedule"] = schedule;
-      schedule({orderId: 120, templateId: 12, relativeTime: 1});
+      // schedule({orderId: 120, templateId: 12, relativeTime: 0.1}); // 任务创建调用测试
       subscriber(null, async ({ orderId, templateId }) => {
         const order = await strapi.query("order").findOne({ id: orderId });
         if (!order) return;
         const { active = false, email = "" } = order;
         if (!active && email) {
           //...发送邮件
-        //   strapi.services["email-template"].sendEmail({ orderid, templateId });
+          strapi.services["email-template"].sendEmail({ orderid, templateId });
         }
       });
     },
